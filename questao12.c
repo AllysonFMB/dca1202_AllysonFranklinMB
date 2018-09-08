9d398a267702be6cffc0a9c49eeb065f23cf7f26
AllysonFMB <33121801+AllysonFMB@users.noreply.github.com>
2018-09-07 22:53:49 -0300

    Create questao12.c

---
+++ questao12.c
@@ -0,0 +1,13 @@
+Comandos válidos:
+aloha[2] = value;
+scanf("%f", &aloha);
+printf("%f", aloha);
+coisas[4][4] = aloha[3];
+pf = aloha;
+
+Comandos inválidos:
+aloha = value";
+coisas[5] = aloha;
+pf = value;	
+
+ 
