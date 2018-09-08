b3db42a38f1e19b19cee22cdebfbe2380376a84a
AllysonFMB <33121801+AllysonFMB@users.noreply.github.com>
2018-09-07 22:18:57 -0300

    Update questao6.c

--- questao6.c
+++ questao6.c
@@ -3,3 +3,5 @@ i = 1vet [1] = 2,2 * (f + 1) = 2,2 & vet [1] = 60FE98 (f + 1) = 60FE98
 i = 2vet [2] = 3,3 * (f + 2) = 3,3 & vet [2] = 60FE9C (f + 2) = 60FE9C
 i = 3vet [3] = 4,4 * (f + 3) = 4,4 & vet [3] = 60FEA0 (f + 3) = 60FEA0
 i = 4vet [4] = 5,5 * (f + 4) = 5,5 e vet [4] = 60FEA4 (f + 4) = 60FEA4
+
+Foram obtidas as respostas esperadas. 
