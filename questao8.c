20326848b23fe9de158f5f2b26c4d33a3dbf3b9c
AllysonFMB <33121801+AllysonFMB@users.noreply.github.com>
2018-09-07 22:27:20 -0300

    Update questao8.c

--- questao8.c
+++ questao8.c
@@ -1,3 +1,10 @@
 As expressões válidas são: 
-p = mat + 1; 
-x = (*mat)++; 
+  p = mat + 1;     
+A variável mat refere-se a posição inicial do vetor (mat[0]), com isso, podendo ser somado com 1 e mudar a posição para 1 (mat[1]).
+  x = (*mat)++; 
+*mat refere-se ao primeiro valor da do vetor mat que pode ser somado com 1;
+
+As expressões inválidas são: 
+  p = mat++;
+  p = ++mat; 
+Em ambas expressões será somado uma unidade de memória no valor atual.
