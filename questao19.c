889e620e42836bd5f8d81c9c1f0e83ad2b1591ea
AllysonFMB <33121801+AllysonFMB@users.noreply.github.com>
2018-09-10 09:22:51 -0300

    Update questao19.c

--- questao19.c
+++ questao19.c
@@ -1,80 +1,93 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-void multMatriz(float *ptrC, float *ptrA, float *ptrB, int nlA, int ncA, int ncB){
+void multMatriz(int **ptrC, int **ptrA, int **ptrB, int nlA, int ncA, int ncB){
     int i,j,k;
-    int *aux;
-    
-    for(i = 0; i< nlA; i++){
-        for(j=0; j<ncB;j++ ){
-            ptrC[i][j] = 0;
+    int aux;
+
+    for(i=0; i<nlA; i++){
+        for(j=0; j<ncB;j++){
+            aux = 0;
             for(k=0;k <ncA; k++){
-                aux += ptrA[i][k] * ptrB[k][j];
+                aux = aux+ ptrA[i][k]*ptrB[k][j];
             }
-            ptrC = aux;
-            aux=0;
+            ptrC[i][j] = aux;
+
         }
     }
 }
-void printando (int vetor[][], int nl, int nc){
+void printando (int **ptrC, int nl, int nc){
     int i,j;
     for(i=0;i<nl; i++){
         for(j=0;j<nc;j++){
-            printf("%d", vetor[i][j]);
+            printf("%d  ", ptrC[i][j]);
         }
         printf("\n");
     }
 }
-int main (){
-    void (*PTR)(float*, float*,float*, int, int, int);
+int main()
+{
+    void (*PTR)(int*, int*,int*, int, int, int);
     PTR = &multMatriz;
     int i,j,k,nlA,ncA,ncB;
-    int *ptrA, *ptrB, *ptrC;
-    
+    int **ptrA, **ptrB, **ptrC;
+
     printf("Informe o numero de linhas da Matriz A: ");
-    scanf("%d", nlA);
+    scanf("%d", &nlA);
     printf("Informe o numero de colunas da Matriz A: ");
-    scanf("%d", ncA);
+    scanf("%d", &ncA);
     printf("Informe o numero de colunas da Matriz B: ");
-    scanf("%d", ncB);
-    
+    scanf("%d", &ncB);
+
     ptrA = (int**)malloc(nlA*sizeof(int*));
     for(i=0; i<nlA; i++){
-        y[i]=(int*)malloc(ncA*sizeof(int));
+        ptrA[i]=(int*)malloc(ncA*sizeof(int));
     }
-    
+
     ptrB = (int**)malloc(ncA*sizeof(int*));
     for(i=0; i<nlA; i++){
-        y[i]=(int*)malloc(ncB*sizeof(int));
+        ptrB[i]=(int*)malloc(ncB*sizeof(int));
     }
-    
+
     ptrC = (int**)malloc(nlA*sizeof(int*));
     for(i=0; i<nlA; i++){
-        y[i]=(int*)malloc(ncB*sizeof(int));
+        ptrC[i]=(int*)malloc(ncB*sizeof(int));
     }
-    
+
     for(i=0;i<nlA;i++){
         for(j=0;j<ncA;j++){
             ptrA[i][j] = rand()%10;
         }
     }
-    
+
     for(i=0;i<ncA;i++){
         for(j=0;j<ncB;j++){
             ptrB[i][j] = rand()%10;
         }
     }
-    
+
     (*PTR)(ptrC, ptrA, ptrB, nlA, ncA, ncB);
-   
+
     printando(ptrA, nlA,ncA);
     printf("\n");
     printando(ptrB, ncA,ncB);
     printf("\n");
     printando(ptrC, nlA,ncB);
-    
+
+    for(i=0; i<nlA; i++){
+      free(ptrA[i]);
+    }
+    for(i=0; i<nlA; i++){
+      free(ptrB[i]);
+    }
+    for(i=0; i<nlA; i++){
+      free(ptrC[i]);
+    }
+
     free(ptrA);
     free(ptrB);
     free(ptrC);
+
     return 0;
 }
+
